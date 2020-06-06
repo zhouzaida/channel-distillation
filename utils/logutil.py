4f9260cb73b798af16746baf31d9af1533c27ebc
zhouzaida <zhouzaida@163.com>
2020-06-06 13:18:14 +0800

    update logutil.py

--- utils/logutil.py
+++ utils/logutil.py
@@ -6,18 +6,15 @@ from logging.handlers import TimedRotatingFileHandler
 def get_logger(name, log_dir='log'):
     if not os.path.exists(log_dir):
         os.makedirs(log_dir)
-
     logger = logging.getLogger(name)
     logger.setLevel(logging.INFO)
-
-    info_name = os.path.join(log_dir, '{}.info.log'.format(name))
-    info_handler = TimedRotatingFileHandler(info_name, when='D', encoding='utf-8')
-    info_handler.setLevel(logging.INFO)
-
+    console_handler = logging.StreamHandler()
+    file_name = os.path.join(log_dir, '{}.info.log'.format(name))
+    file_handler = TimedRotatingFileHandler(file_name, when='D', encoding='utf-8')
     formatter = logging.Formatter('%(asctime)s - %(name)s - %(levelname)s - %(message)s')
+    console_handler.setFormatter(formatter)
+    file_handler.setFormatter(formatter)
 
-    info_handler.setFormatter(formatter)
-
-    logger.addHandler(info_handler)
-
+    logger.addHandler(console_handler)
+    logger.addHandler(file_handler)
     return logger
